f9199495bfeb7c0dfd97a2ccea79d90e5221edc4
tharinm <tharindumadhushan864@gmail.com>
2024-08-17 11:49:03 +0530

    java loop

---
+++ JavaApplication1.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author tharindumadhushan
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+//        Scanner sc = new Scanner(System.in);
+        int a[] = new int[5];
+
+        a[0] = 1;
+        a[1] = 3;
+        a[2] = 4;
+        
+        
+        for(int i=0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+
+        
+        for(int v:a){
+            System.out.println(v);      
+        }
+    }
+
+    private static class doubel {
+
+        public doubel() {
+        }
+    }
+
+}
+Ï
---
+++ Man.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author tharindumadhushan
+ */
+public class Man {
+        String name="Kamal";
+        String address="Panaduta";
+        int number=34343434;
+        
+}
