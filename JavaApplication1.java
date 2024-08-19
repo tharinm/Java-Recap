f95421149d2a85d0bb56b97122499d4b21e98e7b
tharinm <tharindumadhushan864@gmail.com>
2024-08-19 14:50:09 +0530

    linked list

--- JavaApplication1.java
+++ JavaApplication1.java
@@ -7,6 +7,7 @@ package javaapplication1;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 /**
@@ -25,12 +26,26 @@ public class JavaApplication1 {
         users.add(u1);
         users.add(u2);
         users.add(u3);
+//        System.out.println("" +   u2.getName());
 
         for (int i = 0; i < users.size(); i++) {
             System.out.println("User name " + users.get(i).getName());
             System.out.println("User name " + users.get(i).getAge());
 
         }
+
+        System.out.println("---------------------");
+
+        LinkedList<Integer> ages = new LinkedList<>();
+
+        ages.add(30);
+        ages.add(20);
+        ages.add(40);
+        ages.add(50);
+
+        System.out.println("ages " + ages);
+        ages.addFirst(70);
+        System.out.println("ages " + ages);
 
     }
 
